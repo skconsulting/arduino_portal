c49b1d69bb8766736b19aad69240688dd66e4327
Sylvain Kritter <sk_consulting@orange.fr>
2025-09-09 10:23:36 +0200

    Add files via upload

--- rx/rx433_for_portail_test/rx433_for_portail_test.ino
+++ rx/rx433_for_portail_test/rx433_for_portail_test.ino
@@ -1,9 +1,10 @@
 /**********************************************************
   Radiohead_reception_433MHz
   rx 433 test for portail
+  RX DZTZ PIN ON d11
   31 01 2024
 */
-#define RH_ASK_MAX_MESSAGE_LEN 25  //67
+#define RH_ASK_MAX_MESSAGE_LEN 24  //67
 
 #include <RH_ASK.h>  // (fait partie de Radiohead)
 //#define RH_ASK_MAX_PAYLOAD_LEN 20  //67
@@ -21,11 +22,19 @@ uint8_t buflen = sizeof(buf);
 */
 const String stringCAo = String("ouvreCo");
 const String stringCAf = String("fermeCo");
+const unsigned long recdelay = 1000;
+unsigned long startMessage;  // start of message
 //D11 radio input
 bool b = true;
 String myst = "";
 String mysf = "";
 int j = 0;
+int cres = 0;
+String resold = "";
+int longlcd = 17;
+String res = "";
+bool newm = false;
+String newins = "";
 
 String converter(uint8_t *str) {
   return String((char *)str);
@@ -56,8 +65,8 @@ void setup() {
 
 String padStringTo16(String ins) {
   String str = ins;
-  if (str.length() < 17) {
-    while (str.length() < 17) {
+  if (str.length() < longlcd) {
+    while (str.length() < longlcd) {
       str = str + " ";
     }
   }
@@ -76,40 +85,59 @@ void loop1() {
 }
 void loop() {
   int i;
-  String res = "";
-  String newins = "";
+  //String res = "";
+
 
   uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
   uint8_t buflen = sizeof(buf);
 
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  /* digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(200);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
   delay(200);
-
+*/
   if (driver.recv(buf, &buflen)) {
-    Serial.print("Receive message:" + String(j) + " length: " + String(buflen) + ": ");
+    res = "";
+    newm = true;
+    //Serial.print("Receive message:" + String(j) + " length: " + String(buflen) + ": ");
     for (i = 0; i < buflen; i++) {
-      Serial.write(buf[i]);
+      //Serial.write(buf[i]);
       res = res + char(buf[i]);
     }
-    
 
-    //b = !b;
+    //Serial.println();
+    if (resold != res) {
+      startMessage = millis();
+      resold = res;
+      cres = 1;
 
-
-    if ((stringCAo == res) || (stringCAf == res)) {
-      newins = padStringTo16(res);
+      newins = padStringTo16(String(j) + ": " + res + " new");
       lcd.setCursor(0, 0);  // set the cursor to column 0, line 0
-      lcd.print(String(j)+": "+newins);
+      lcd.print(newins);
+      Serial.println("Message recu 0 0: " + res + " new");
     } else {
-      lcd.setCursor(0, 1);  // set the cursor to column 0, line 0
-      newins = padStringTo16(res);
-      lcd.print(String(j)+": "+newins);
+      cres++;
+      Serial.println(String(millis() - startMessage) + " ms");
     }
-    Serial.println("Message recu: " + res);
+  }
+  if (((millis() - startMessage) > recdelay) && newm) {
+    newm = false;
+    startMessage = millis();
+    if ((stringCAo == resold) || (stringCAf == resold) || res.startsWith("T") || res.startsWith("H")) {
+      newins = padStringTo16(String(j) + ": " + resold + " " + String(cres));
+      lcd.setCursor(0, 0);  // set the cursor to column 0, line 0
+      lcd.print(newins);
+      Serial.println("Message recu 0 0: " + resold + " " + String(cres));
+    } else {
+      newins = padStringTo16(String(j) + ": " + resold + " " + String(cres));
+      lcd.setCursor(0, 1);  // set the cursor to column 0, line 1
+      lcd.print(newins);
+      Serial.println("Message recu 0 1: " + resold + " " + String(cres));
+    }
+
     j++;
-    /*
+  }
+  /*
     String subt = myst.substring(0, 6);
     //    Serial.println(sub.substring(0, 1));
     
@@ -117,5 +145,4 @@ void loop() {
     //    Serial.println(sub.substring(0, 1));
     Serial.println("Message recuf: " + subf);
     */
-  }
 }
